8d071bb5b45fb4efee1af082769bb7f5598e16ec
jashley2017 <jashley2017@gmail.com>
2020-02-04 22:19:57 -0500

    added neovim config

--- installme.sh
+++ installme.sh
@@ -8,6 +8,7 @@ mv -f ~/.bash_logout old/bash_logout.old
 mv -f ~/.bash_profile old/bash_profile.old
 mv -f ~/.vimrc old/.vimrc_old
 mv -f ~/.vim old/.vim_old
+mv -f ~/.config/nvim old/config_nvim_old
 
 # link the new ones
 ln -s $PWD/bashrc.sh ~/.bashrc
@@ -17,6 +18,7 @@ ln -s $PWD/bash_profile.sh ~/.bash_profile
 ln -s $PWD/bashrc.sh ~/.bashrc
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/vim ~/.vim
+ln -s $PWD/config/nvim ~/.config/nvim
 
 # protect the directory once they are linked
 chmod u-w ../dotfiles 
