0ea2d5e8f678b829c7c084e5e0e6a9301de6396f
jashley2017 <jashley2017@gmail.com>
2020-02-04 22:16:14 -0500

    update install for vim

--- installme.sh
+++ installme.sh
@@ -7,6 +7,7 @@ mv -f ~/.bash_aliases old/bash_aliases.old
 mv -f ~/.bash_logout old/bash_logout.old
 mv -f ~/.bash_profile old/bash_profile.old
 mv -f ~/.vimrc old/.vimrc_old
+mv -f ~/.vim old/.vim_old
 
 # link the new ones
 ln -s $PWD/bashrc.sh ~/.bashrc
@@ -15,6 +16,7 @@ ln -s $PWD/bash_logout.sh ~/.bash_logout
 ln -s $PWD/bash_profile.sh ~/.bash_profile
 ln -s $PWD/bashrc.sh ~/.bashrc
 ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/vim ~/.vim
 
 # protect the directory once they are linked
 chmod u-w ../dotfiles 
